216907e5c3b6c23346a9c380e3bbc18359dba8d2
JmAtochePascual <jmatochepascualgmail.com>
2025-12-08 16:29:26 -0500

    feat: :sparkles: Add Stats component and integrate with main page

---
+++ src/data/stasts.ts
@@ -0,0 +1,19 @@
+interface StatItem {
+	value: string;
+	label: string;
+}
+
+export const STATS_DATA: StatItem[] = [
+	{
+		value: '10 Años',
+		label: 'De experiencia en transporte interprovincial.',
+	},
+	{
+		value: '100%',
+		label: 'Monitoreo GPS y seguridad las 24 horas.',
+	},
+	{
+		value: '160°',
+		label: 'Reclinación máxima en nuestro servicio EcoVip.',
+	},
+];
